cf23975cbd08c36302e4c3135d9403f93f8066ed
Codrut Befu <codrut_befu@yahoo.com>
2025-11-23 20:27:17 +0200

    casa

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -8,8 +8,19 @@ class ConfigController extends Controller
 {
     public function config()
     {
+        $ip = $_SERVER['REMOTE_ADDR'] ?? null;
+        
+        $casa = 1; // Default value
+        
+        if ($ip !== null) {
+            $casaConfig = config('casa.casa');
+            if (isset($casaConfig[$ip])) {
+                $casa = $casaConfig[$ip];
+            }
+        }
+        
         $config = [
-            'casa' => 2
+            'casa' => $casa
         ];
 
         return response()->json($config);
